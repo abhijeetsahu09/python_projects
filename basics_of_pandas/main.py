122e44a2ad4be06049ce752cca60456375efd0ad
Abhijeet Sahu <abhijeetsahu937@gmail.com>
2024-01-10 22:04:54 +0530

    List Comprehension and dictionary comprehension file added

---
+++ basics_of_pandas/main.py
@@ -0,0 +1,74 @@
+# with open("weather_data.csv") as data_file:
+#     data = data_file.readlines()
+#     print(data)
+
+# import csv
+#
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperatures = []
+#     for row in data:
+#         if row[1] != "temp":
+#             temperatures.append(int(row[1]))
+#     print(temperatures)
+
+# import pandas
+#
+# data = pandas.read_csv("weather_data.csv")
+# print(type(data))
+# print(data["temp"])
+
+# data_dict = data.to_dict()
+# print(data_dict)
+#
+# temp_list = data["temp"].to_list()
+# print(len(temp_list))
+#
+# print(data["temp"].mean())
+# print(data["temp"].max())
+#
+# #GEt Data in Columns
+# print(data["condition"])
+# print(data.condition)
+
+#Get Data in Row
+# print(data[data.day == "Monday"])
+# print(data[data.temp == data.temp.max()])
+
+# monday = data[data.day == "Monday"]
+# monday_temp = monday.temp[0]
+# monday_temp_F = monday_temp * 9/5 + 32
+# print(monday_temp_F)
+
+#Create a dataframe from scratch
+# data_dict = {
+#     "students": ["Army", "James", "Abhijeet"],
+#     "scores": [76, 56, 65]
+# }
+# data = pandas.DataFrame(data_dict)
+# data.to_csv("new_data.csv")
+
+import pandas
+
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+grey_squirrel_count = len(data[data["Primary Fur Color"] == "Gray"])
+red_squirrel_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrel_count = len(data[data["Primary Fur Color"] == "Black"])
+
+print(grey_squirrel_count)
+print(red_squirrel_count)
+print(black_squirrel_count)
+
+data_dict = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [grey_squirrel_count, red_squirrel_count, black_squirrel_count]
+}
+
+df = pandas.DataFrame(data_dict)
+df.to_csv("squirrel_count.csv")
+
+
+
+
+
+
